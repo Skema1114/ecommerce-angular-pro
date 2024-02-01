0d0a4830900bb75842e215bde043143cc8ddab5a
Rafael Aranda Martins <skema1114@hotmail.com>
2024-01-31 21:02:44 -0300

    feat(layout): add header component;

---
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecommerce-angular-pro-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
