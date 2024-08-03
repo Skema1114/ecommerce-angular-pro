a1f621bbc5a871660449c824bbaa62540f3e76b4
Rafael Aranda Martins <skema1114@hotmail.com>
2024-08-03 19:15:17 -0300

    feat(data-access | #21): add user-list service;

--- libs/modules/data-access/product/src/lib/product-search/product-search.service.ts
+++ libs/modules/data-access/product/src/lib/product-search/product-search.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Product } from '../models/product.model';
@@ -10,7 +10,9 @@ import { Product } from '../models/product.model';
 export class ProductSearchService {
   readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io';
 
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
+
+  constructor() {}
 
   searchByName(name: string): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.apiUrl}/products`, {
---
+++ libs/modules/data-access/users/src/lib/users-list/users-list.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/users.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersListService {
+  readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io/users';
+
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  listUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users/`);
+  }
+}
