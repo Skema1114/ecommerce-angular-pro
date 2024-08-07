e1fb410191e7fd3f93d151bb3e433be841d09143
Moraes Neto <moraes@dbsystem.com.br>
2024-08-07 14:17:46 -0300

    fix(app | #45): import error adjustment.

--- apps/ecommerce-angular-pro/src/app/app.component.spec.ts
+++ apps/ecommerce-angular-pro/src/app/app.component.spec.ts
@@ -2,19 +2,13 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        AppComponent,
-        NxWelcomeComponent,
-        RouterTestingModule,
-        HttpClientTestingModule,
-      ],
+      imports: [AppComponent, RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
