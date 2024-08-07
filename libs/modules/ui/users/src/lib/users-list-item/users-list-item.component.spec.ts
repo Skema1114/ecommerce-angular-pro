dd904edd4c688c7f7ba01c994decf94b3dcb04b8
Moraes Neto <moraes@dbsystem.com.br>
2024-08-07 14:11:12 -0300

    test(users-list | #45): tesr render user card.

--- libs/modules/ui/users/src/lib/users-list-item/users-list-item.component.spec.ts
+++ libs/modules/ui/users/src/lib/users-list-item/users-list-item.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { mockUsers } from '@ecommerce-admin/users-data-access';
 import { UsersListItemComponent } from './users-list-item.component';
 
 describe('UsersListItemComponent', () => {
@@ -7,15 +9,23 @@ describe('UsersListItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UsersListItemComponent],
+      imports: [UsersListItemComponent, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UsersListItemComponent);
     component = fixture.componentInstance;
+    component.data = mockUsers[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render user card info correctly', () => {
+    const card: HTMLElement = fixture.nativeElement.querySelector('mat-card');
+    expect(card.textContent).toContain(component.data.name);
+    expect(card.textContent).toContain(component.data.biography);
+    expect(card.textContent).toContain(component.data.email);
   });
 });
