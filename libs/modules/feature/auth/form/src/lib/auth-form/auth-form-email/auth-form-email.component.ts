d94e2539fc670db3629a6f001b55c2d93ebfed1e
Rafael Aranda Martins <skema1114@hotmail.com>
2024-08-07 10:38:01 -0300

    refactor(feature | #35): ajustado visual e add botões de voltar e sair;

--- libs/modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
+++ libs/modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { AuthFormComponent } from '../auth-form.component';
@@ -15,6 +17,8 @@ import { AuthFormComponent } from '../auth-form.component';
     MatInputModule,
     ReactiveFormsModule,
     RouterModule,
+    MatFormFieldModule,
+    MatIconModule,
   ],
   templateUrl: './auth-form-email.component.html',
   styleUrl: './auth-form-email.component.scss',
